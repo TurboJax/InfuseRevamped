47a08f910365ff2eee5b4c10c6041b2a2314ebc7
turbojax07 <turbojax07@gmail.com>
2026-01-03 05:20:40 -0500

    Beginnings

---
+++ src/main/java/org/turbojax/Infuse.java
@@ -0,0 +1,15 @@
+package org.turbojax;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Infuse extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
