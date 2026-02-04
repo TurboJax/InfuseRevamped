ba376f0d297659584b99a0bc05eb6ef8177643f7
TurboJax07 <turbojax07@gmail.com>
2026-02-04 12:15:38 -0500

    Implementing basic data management for the effects

--- src/main/java/org/turbojax/data/DataManager.java
+++ src/main/java/org/turbojax/data/DataManager.java
@@ -1,9 +1,35 @@
 package org.turbojax.data;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.turbojax.effects.InfuseEffect;
 
 public class DataManager extends TurboConfig {
     public DataManager(Plugin plugin) {
         super(plugin, "data/playerdata.yml");
+    }
+
+    public InfuseEffect getLeftEffect(Player player) {
+        return InfuseEffect.deserialize(config.getString(player.getUniqueId() + ".left"));
+    }
+
+    public void setLeftEffect(Player player, InfuseEffect effect) {
+        config.set(player.getUniqueId() + ".left", effect.serialize());
+    }
+
+    public InfuseEffect getRightEffect(Player player) {
+        return InfuseEffect.deserialize(config.getString(player.getUniqueId() + ".right"));
+    }
+
+    public void setRightEffect(Player player, InfuseEffect effect) {
+        config.set(player.getUniqueId() + ".right", effect.serialize());
+    }
+
+    public boolean hasEffect(Player player, InfuseEffect effect) {
+        InfuseEffect leftEffect = getLeftEffect(player);
+        InfuseEffect rightEffect = getRightEffect(player);
+        Class<?> effectClass = effect.getClass();
+
+        return effectClass.isInstance(leftEffect) || effectClass.isInstance(rightEffect);
     }
 }
