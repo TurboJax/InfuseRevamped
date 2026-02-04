6b38959b6ed73e212abdc4658919483fc84b852c
TurboJax07 <turbojax07@gmail.com>
2026-02-04 12:07:53 -0500

    Adding the InfuseEffect interface

---
+++ src/main/java/org/turbojax/effects/InfuseEffect.java
@@ -0,0 +1,10 @@
+package org.turbojax.effects;
+
+public interface InfuseEffect {
+    public String serialize();
+
+    public static InfuseEffect deserialize(String serializedEffect) {
+        // TODO: Implement
+        return null;
+    }
+}
