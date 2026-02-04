abb48a688acdc57b5e8bdcc84cd757694f564db3
TurboJax07 <turbojax07@gmail.com>
2026-02-04 13:53:56 -0500

    Fleshing out the InfuseEffect class and adding FrostEffect by name only

---
+++ src/main/java/org/turbojax/effects/FrostEffect.java
@@ -0,0 +1,7 @@
+package org.turbojax.effects;
+
+public class FrostEffect extends InfuseEffect {
+    public FrostEffect(boolean augmented) {
+        super("frost", augmented);
+    }
+}
--- src/main/java/org/turbojax/effects/InfuseEffect.java
+++ src/main/java/org/turbojax/effects/InfuseEffect.java
@@ -1,20 +1,34 @@
 package org.turbojax.effects;
 
 public abstract class InfuseEffect {
+    protected final String name;
     protected final boolean augmented;
 
-    public InfuseEffect(boolean augmented) {
+    public InfuseEffect(String name, boolean augmented) {
+        this.name = name;
         this.augmented = augmented;
     }
 
-    public boolean isAugmented() {
+    public boolean getAugmented() {
         return augmented;
     }
 
-    public abstract String serialize();
+    public String getName() {
+        return name;
+    }
+
+    public String serialize() {
+        return (augmented ? "aug_" : "") + name;
+    }
 
     public static InfuseEffect deserialize(String serializedEffect) {
-        // TODO: Implement
+        boolean augmented = serializedEffect.startsWith("aug_");
+        serializedEffect = serializedEffect.replace("aug_", "");
+
+        switch (serializedEffect) {
+            case "frost":
+                return new FrostEffect(augmented);
+        }
         return null;
     }
 }
