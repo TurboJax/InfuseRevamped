73ec55a0139a76df838ded8a5ec8cbfe4fe195a7
TurboJax <turbojax07@gmail.com>
2026-02-13 21:54:07 -0500

    Adding the EffectConstants class to track effect-specific constants

---
+++ src/main/java/org/turbojax/EffectConstants.java
@@ -0,0 +1,8 @@
+package org.turbojax;
+
+import org.turbojax.effects.InfuseEffect;
+
+/**
+ * This class allows you to get constant values that differ for each {@link InfuseEffect}.
+ */
+public class EffectConstants {}
